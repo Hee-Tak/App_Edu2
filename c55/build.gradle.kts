3a7b489f456ac827409be2a8c79ba9fe3b371d93
hitak <hitak77@naver.com>
2024-01-08 15:29:50 +0900

    앱 개발 학습 (인강) - 55) 앱 데이터 저장 (SharedPreference -> XML File)

--- c55/build.gradle.kts
+++ c55/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.tak.c55"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.tak.c55"
--- c55/src/main/java/com/tak/c55/MainActivity.kt
+++ c55/src/main/java/com/tak/c55/MainActivity.kt
@@ -1,12 +1,39 @@
 package com.tak.c55
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val editView = findViewById<EditText>(R.id.editView)
+        val checkView = findViewById<CheckBox>(R.id.checkView)
+        val saveButton = findViewById<Button>(R.id.saveButton)
+        val getButton = findViewById<Button>(R.id.getButton)
+        val resultView = findViewById<TextView>(R.id.resultView)
+
+        val sharedPref = getSharedPreferences("my_prefs", Context.MODE_PRIVATE)
+
+        saveButton.setOnClickListener {
+            sharedPref.edit().run {
+                putString("data1", editView.text.toString())
+                putBoolean("data2", checkView.isChecked)
+                commit()
+            }
+        }
+
+        getButton.setOnClickListener {
+            val data1 = sharedPref.getString("data1", "none")
+            val data2 = sharedPref.getBoolean("data2", false)
+            resultView.text = "data1: $data1,  data2: $data2"
+        }
     }
 }
 
@@ -59,4 +86,42 @@ class MainActivity : AppCompatActivity() {
  *      val writeStream: OutputStreamWriter = file.writer()
  *      writeStream.write("hello world")
  *      writeStream.flush()
+ */
+
+/**
+ * SharedPreference
+ *
+ * - 데이터를 키-값 형태로 저장 => 그렇기 때문에 SharedPreference 를 Map 객체 정도라고 가정을 해서 이용해도 됨
+ * - 내부적으로 내장 메모리의 앱 폴더에 XML 파일로 데이터가 저장 (내부적으로 데이터를 영속적으로 저장함 => 파일에다가 (xml파일))
+ * - 직접 read/write 를 하는 구조가 아니라 알아서 read/write 하는 구조라고 함
+ * - 이미지 데이터 저장에는 부적절함
+ * - 내장 메모리 공간에 저장된다 => 해당하는 그 앱에서만 사용할 수 있다.
+ *
+ *
+ * - Activity.getPreferences (int mode)
+ * - 하나의 액티비티를 위한 데이터 저장을 목적
+ * - 이 함수를 이용한 액티비티 클래스명으로 XML 파일명이 만들어 짐
+ *
+ *
+ * - Context.getSharedPreferences (String name, int mode)
+ * - 앱 전체의 데이터를 키-값 형태로 저장      => 앱 전역을 위한 데이터가 된다.
+ *      val sharedPref = getSharedPreferences("my_prefs", Context.MODE_PRIVATE)
+ *
+ * - 데이터를 저장하기 위해서는 SharedPreferences.Editor 클래스의 함수를 이용
+ * - SharedPreferences.Editor 객체는 SharedPreferences 의 edit() 함수로 획득
+ * - commit() 하는 순간 저장
+ *      putBoolean(String key, boolean value)
+ *      putInt(String key, int value)
+ *      putFloat(String key, float value)
+ *      putLong(String key, long value)
+ *      putString(String key, String value)
+ *
+ *
+ * - 데이터 획득은 SharedPreferences 의 getter 함수를 이용
+ *      getBoolean(String key, boolean defValue)
+ *      getFloat(String key, float defValue)
+ *      getInt(String key, int defValue)
+ *      getLong(String key, long defValue)
+ *      getString(String key, String defValue)
+ *
  */
