b3afa343f7138c0865f554471e03c9b97b9691a5
hitak <hitak77@naver.com>
2024-01-20 19:21:31 +0900

    앱 개발 학습 (인강) - C60) rawQuery, execSql

--- c60/src/main/java/com/tak/c60/MainActivity.kt
+++ c60/src/main/java/com/tak/c60/MainActivity.kt
@@ -11,5 +11,12 @@ class MainActivity : AppCompatActivity() {
 }
 
 /**
- * 바빠서 잠시 보류;;
+ * C60
+ * <rawQuery, execSql>
+ *
+ * <SQLiteDatabase>
+ * - SQLite를 이용하기 위한 최소한의 API
+ * - openOrCreateDatabase() 함수를 이용해 획득
+ * - SQLiteOpenHelper 객체를 이용해 획득
+ *      val db = openOrCreateDatabase("testdb", Context.MODE_PRIVATE,null)
  */
