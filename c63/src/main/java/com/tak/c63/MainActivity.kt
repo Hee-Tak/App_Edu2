5e900285d2f1833cb085e53405bd58e30a12920e
hitak <hitak77@naver.com>
2024-01-25 19:51:36 +0900

    앱 개발 학습 (인강)

---
+++ c63/src/main/java/com/tak/c63/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.tak.c63
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
+
+/**
+ * 구글의 다양한 앱을 연동해 보자
+ * Concepts of ContentProvider
+ */
