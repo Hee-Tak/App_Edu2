013bb063ec8c33e8689785a434b124f247e47a9f
hitak <hitak77@naver.com>
2024-01-27 04:53:09 +0900

    앱 개발 학습 (인강) 63강 : Gallery App (사진 앱)

---
+++ c64/src/main/java/com/tak/c64/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.tak.c64
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
+
+/**
+ * C64
+ * <Gallery App>
+ */
