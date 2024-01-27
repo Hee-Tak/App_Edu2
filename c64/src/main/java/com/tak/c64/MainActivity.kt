d42879e05a04136d9c794fddfc631b2806abede7
hitak <hitak77@naver.com>
2024-01-28 04:41:51 +0900

    앱 개발 학습 (인강) 63강 : Gallery App (사진 앱)

--- c64/src/main/java/com/tak/c64/MainActivity.kt
+++ c64/src/main/java/com/tak/c64/MainActivity.kt
@@ -13,4 +13,10 @@ class MainActivity : AppCompatActivity() {
 /**
  * C64
  * <Gallery App>
+ *
+ *
+ * - 인텐트로 갤러리앱의 이미지 목록 화면을 출력
+ *          val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+ *          intent.type = "image/*"
+ *          requestActivity.launch(intent)
  */
