deae0041c1bc7147182be596b15d18074cc5205f
hitak <hitak77@naver.com>
2024-01-29 05:46:50 +0900

    앱 개발 학습 (인강) / C66) Camera App

---
+++ c66/src/main/java/com/tak/c66/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.tak.c66
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
+
+/**
+ * C66
+ *
+ */
