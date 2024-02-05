98f67141c2e8b6385e05e9814983a053be5cb224
hitak <hitak77@naver.com>
2024-02-06 08:59:32 +0900

    앱 개발 학습 (인강) / bindService()

--- c72/src/main/java/com/tak/c72/MainActivity.kt
+++ c72/src/main/java/com/tak/c72/MainActivity.kt
@@ -30,4 +30,24 @@ class MainActivity : AppCompatActivity() {
  *          |       ↓       |                                   |               |
  *          |               |                                   |               |
  *          -----------------                                   -----------------
+ *
+ *          이런 식으로 두 컴포넌트 간에 매개변수나 리턴값으로 데이터를 주고 받을 수 있다.
+ *
+ * - 서비스의 라이프사이클 함수중 onBind() 함수가 실행
+ *
+ *          class MyBinder: Binder() {
+ *              fun funA(arg: Int) {
+ *
+ *              }
+ *
+ *              fun funB(arg: Int): Int {
+ *                  return arg * arg
+ *              }
+ *          }
+ *
+ *          override fun onBind(intent: Intent): IBinder? {
+ *              return MyBinder()
+ *          }
+ *          // bindService() 에 의해서 onBind()가 호출이 되면, (IBinder) 객체를 리턴시켜 줘야되고, 이 객체가 액티비티 같은 곳에 전달된다는 것.
+ *
  */
