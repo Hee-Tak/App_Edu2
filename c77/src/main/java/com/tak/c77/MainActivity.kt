fa5a2d52fa4230085a43d64f0a424791b0af954b
hitak <hitak77@naver.com>
2024-02-10 04:07:42 +0900

    앱 개발 학습 (인강) / Background Limit

---
+++ c77/src/main/java/com/tak/c77/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.tak.c77
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
+
+
+/**
+ * 22. Background Limit 문제를 해결해 보자.
+ * <Concepts of Background Limit>
+ */
