068cae8350c084e2ef4f30bc93555defb7f1ea7a
hitak <hitak77@naver.com>
2024-02-09 23:07:07 +0900

    앱 개발 학습 (인강) / Notification 구성

--- c75/src/main/java/com/tak/c75/MainActivity.kt
+++ c75/src/main/java/com/tak/c75/MainActivity.kt
@@ -1,12 +1,72 @@
 package com.tak.c75
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.graphics.BitmapFactory
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.core.app.NotificationCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener {
+            val manager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            val builder: NotificationCompat.Builder
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                val channelId = "one"
+                val channelName = "channel name"
+                val channel = NotificationChannel(
+                    channelId,
+                    channelName,
+                    NotificationManager.IMPORTANCE_HIGH
+                )
+                channel.description = "one description"
+                manager.createNotificationChannel(channel) //manager 에 채널 등록
+
+                builder = NotificationCompat.Builder(this, channelId)
+            } else {
+                builder = NotificationCompat.Builder(this)
+            }
+            //여기까지 빌더 만듦
+
+
+            builder.setSmallIcon(android.R.drawable.ic_notification_overlay)
+            builder.setWhen(System.currentTimeMillis())
+            builder.setContentText("Text")
+            builder.setContentTitle("Title")
+
+            val actionIntent = Intent(this, DetailActivity::class.java)
+            val actionPendingIntent = PendingIntent.getActivity(this, 20, actionIntent, PendingIntent.FLAG_UPDATE_CURRENT)
+            builder.setContentIntent(actionPendingIntent)
+
+            val actionIntent2 = Intent(this, DetailActivity::class.java)
+            val actionPendingIntent2 = PendingIntent.getActivity(this, 20, actionIntent, PendingIntent.FLAG_UPDATE_CURRENT)
+            builder.addAction(
+                NotificationCompat.Action.Builder(
+                    android.R.drawable.stat_notify_more,
+                    "Action",
+                    actionPendingIntent2
+                ).build()
+            )
+
+            val bigPicture = BitmapFactory.decodeResource(resources, R.drawable.logo_1)
+            val style = NotificationCompat.BigPictureStyle()
+            style.bigPicture(bigPicture)
+            builder.setStyle(style)
+
+            manager.notify(1, builder.build())
+
+        }
+
     }
 }
 
