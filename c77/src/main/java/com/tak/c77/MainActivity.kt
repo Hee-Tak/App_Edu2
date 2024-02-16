fdc570ebe1eee1d5914d53df8d41bdbc750e6f9b
hitak <hitak77@naver.com>
2024-02-17 02:46:00 +0900

    앱 개발 학습 (인강) / Background Limit - Service Limit

--- c77/src/main/java/com/tak/c77/MainActivity.kt
+++ c77/src/main/java/com/tak/c77/MainActivity.kt
@@ -106,13 +106,79 @@ class MainActivity : AppCompatActivity() {
  * 78강 : Service Limit
  *
  * <Service Limit>
- *
- * - 앱이 백그라운드 상태에 있을 때 서비스를 실행시키기 위해서 인텐트를 발생시키면 에러 발생
+ *                                                                                  (브로드캐스트와는 다르게)
+ * - 앱이 백그라운드 상태에 있을 때 서비스를 실행시키기 위해서 인텐트를 발생시키면 에러 발생 => 명시적 인텐트, 암시적 인텐트 모두 다 에러.
  * - 앱이 포그라운드 상황이라면 정상 실행
  *
  *          Not allowed to start service Intent { act=ACTION_OUTER_SERVICE pkg=com.example.test }: app is in background uid null
  *
  *
+ *
+ *
+ * * 꼭 대놓고 포그라운드 상황만 보는게아니라, 몇가지 경우를 포그라운드 상황으로 봐주는 경우가 있다.
+ *
+ * - Activity 가 시작되거나 일시 중지되거나 상관없이 보이는 Activity 가 있는 경우 (포그라운드 상황)
+ *
+ * - 포그라운드 서비스가 있는 경우 (즉, 여러 개의 서비스 중에 포그라운드 서비스가 존재할 경우. 다른것도 구동될 수 있다는 걸 뜻함)
+ * - 앱의 서비스 중 하나에 바인드하거나, 앱의 콘텐츠 프로바이더 중 하나를 사용하여 앱에 또 다른 포그라운드 앱이 연결된 경우
+ *      (다른 포그라운드 앱과 연결되어 사용되는 경우) (이 연결은 앱의 콘텐츠 프로바이더 중 하나를 사용하여 연결)
+ *      다른 앱이, 우리의 서비스를 bindService 방식으로 이용하고 있다면
+ *      우리 앱은 백그라운드긴 하지만, 우리 앱과 연동하고 있는 다른 쪽에 앱이 포그라운드 상황일 수 있는 것.
+ *      우리 앱과 연동하면서 데이터를 그쪽에 뿌릴 수 있어서, 결국 우리 데이터가 화면에 나올 수가 있다는 것. 이럴 때는 포그라운드 상황으로 봐줌.
+ *
+ *      혹은 컨텐트 프로바이더도 마찬가지.
+ *      컨텐트 프로바이더를 다른 쪽에서 이용하고 있다면, 우리 앱은 백그라운드 상황이지만, 다른 쪽 앱에서 우리 앱의 데이터가 뿌려질 수가 있어서
+ *      포그라운드 상황으로 보고, 이런 경우에는 서비스가 동작이 된다.
+ *
+ *
+ *
+ *
+ * - 앱이 백그라운드 상황에 있다고 하더라도 아래의 경우에는 서비스가 정상적으로 실행
+ *
+ * - 우선순위가 높은 Firebase 클라우드 메시징 (FCM) 메시지 처리
+ * - SMS/MMS 메시지와 같은 브로드캐스트 수신
+ * - 알림에서 PendingIntent 실행 (Notification) => 특히 이 알림에서는 우리 앱의 정보가 뿌려질 수 있으니 그런듯.
+ * - VPN 앱이 포그라운드로 승격되기 전에 VpnService 시작
+ *
+ *
+ *
+ *
+ *
+ * - 백그라운드 상황에서 서비스가 정상 실행되게 할 수 있는 방법
+ * - startForegroundService() 함수에 의해 인텐트를 발생시키면 앱이 백그라운드 상황이라고 하더라도 정상적으로 서비스가 실행
+ *
+ *          if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+ *              startForegroundService(intent)
+ *          } else {
+ *              startService(intent)
+ *          }
+ *
+ * * 이것과 관련된 가장 쉬운 테스트
+ *      => 폰에 boot complete 시에, 우리의 리시버가 실행되게 만들고,
+ *          그런 다음에, 브로드캐스트 리시버가 서비스를 구동시켜 버리면
+ *          그러면 부팅이 완료되자마자 서비스가 구동되는 거니까, 이 앱은 완벽하게 백그라운드 상황이다.
+ *          그 상황에서 브로드캐스트 리시버가 서비스를 실행시켜서 인텐트를 발생시키면 실행이 안된다는 것.
+ *
+ *          그런데 브로드캐스트 리시버가 서비스를 실행시키면서 startForegroundService 를 가지고 실행을 시켰다고 하면
+ *          정상적으로 실행이 된다.
+ *
+ *          근데 이 함수에는 조건이 붙음. 일단은 실행이 되기는 되는데
+ *          아주 빠른 시간 내에 뭔가를 띄워줘야함. 그러지 않으면 에러가 발생한다.
+ *
+ * - startForegroundService() 함수에 의해 서비스가 정상 실행되기는 하지만 얼마 후 에러가 발생
+ *
+ *          Context.startForegroundService() did not then call Service.startForeground()
+ *          => 뭔가를 call 하지 않았다.
+ *
+ * * startForeground() 에서 () 안에 들어갈 매개변수는 Notification.
+ *
+ * - startForegroundService() 에 의해 실행된 서비스 내에서 빠른 시간내에 startForeground() 함수를 호출해 줘야 한다.
+ * - startForeground() 함수의 매개변수는 알림 객체 (Notification)
+ *
+ *          val notification = builder.build()
+ *          startForeground(1, notification)            //이 경우에는 서비스가 구동이 되자마자 빠르게 notification을 띄워줘야한다는 얘기
+ *
+ *          <uses-permission android:name="android.permission.FOREGROUND_SERVICE"/>
  *
  */
 
