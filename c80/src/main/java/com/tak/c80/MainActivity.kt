398f60310e21a4289c7ff5b719a6120f746af175
hitak <hitak77@naver.com>
2024-02-18 13:35:57 +0900

    앱 개발 학습 (인강) / GeoLocation

---
+++ c80/src/main/java/com/tak/c80/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.tak.c80
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
+
+/**
+ * 23. 유저 위치 정보를 앱에 적용해 보자.
+ *
+ * C80) Concepts of GeoLocation
+ */
