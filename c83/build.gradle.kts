39c8543f302df3f94954054c905a798cf384a044
hitak <hitak77@naver.com>
2024-02-24 16:54:56 +0900

    앱 개발 학습 (인강) / GeoLocation - GoogleMap

--- c83/build.gradle.kts
+++ c83/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.tak.c83"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.tak.c83"
@@ -44,4 +44,11 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    //implementation("com.google.android.gms:play-services:12.0.1") //원래 실습따라가면 얘를 쓰는데, 얘를 그대로 쓰면 Duplicate class found 오류 발생. 클래스 중복문제
+    // Google Play 서비스의 Location 모듈 추가
+    implementation("com.google.android.gms:play-services-location:18.0.0")
+
+    // Google Maps 모듈 추가
+    implementation("com.google.android.gms:play-services-maps:18.0.0")
 }
--- c83/src/main/java/com/tak/c83/MainActivity.kt
+++ c83/src/main/java/com/tak/c83/MainActivity.kt
@@ -2,15 +2,70 @@ package com.tak.c83
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
 
-class MainActivity : AppCompatActivity() {
+
+//지도라는게 콜백으로 전달된다. 그래서 인터페이스를 이쪽에서 구현한다면서 아래에 AppCompatActivity() 부분 옆에 추가 OnMapReadyCallback 추가
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    var googleMap: GoogleMap? = null    //지도 객체
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        (supportFragmentManager.findFragmentById(R.id.mapView) as SupportMapFragment)!!.getMapAsync(this)   // 콜백함수가 현 클래스에 이렇게 등록함. 그러면 이제 콜백 함수를 오버라이드 받아서 작성해놓으면 되겠지. 그게 onMapReady
+    }
+
+    override fun onMapReady(p0: GoogleMap) {
+        //이 함수가 호출이 되었다라는 것은 지도 객체를 우리 앱에서 이용이 가능하다는 얘기. 그래서 이 함수의 매개변수가 지도 객체가 되겠고
+        googleMap = p0
+        val latLng = LatLng(37.566610, 126.978403) //임의로 지정. (서울 시청값이란다) (서울 시청을 센터로 잡기 위한 위경도 값)
+        val position = CameraPosition.Builder() //카메라라는 표현을 썼지만, 지도화면이라 보면되고, 지도 화면을 설정하기 위한 정보 라고 보면된다.
+            .target(latLng)     //센터위치설정
+            .zoom(16f)             //초기 지도가 뜨는 줌 레벨
+            .build()                //여기까지 준비한건 정보. 이제 이 정보대로 지도에 센터가 보이게끔 명령을 내려야함.
+
+        googleMap?.moveCamera(CameraUpdateFactory.newCameraPosition(position)) //이렇게 하면, 우리가 원하는 위치를 센터로 해서 지도를 뿌릴 수가 있다.
     }
 }
+//===============================================================================================================================================================================================
+        //코드는 여기까지인데, 마지막으로 API_KEY 등록까지 해야함. (매니페스트 파일에서) -> 구글 개발자 사이트에서 PC에서 SHA1 지문을 얻어야됨 -> 이 지문을 어플리케이션 사인했던 키값으로 얻어내면 된다.
+        //근데 테스트용으로는 안드로이드에서 그냥 클릭 클릭해서 된다고 함
+        //일단 이 작업 자체는 File->Settings->(맨마지막부분인)Experimental->Gradle의 Do not build Gradle task list during Gradle sync 체크박스 해제 -> 그런다음에, 다시 gradle sync
+        //그다음 안드로이드 스튜디오 좌측측면 탭에 Gradle -> 해당 모듈 -> Tasks -> android -> signingReport 에 가야하는데 지금 난, android 가 없음. 다른 방법을 알아봐야함.
 
+        //근데 22.3.1 패치 이후에는 Configure all Gradle tasks during Gradle sync 얘를 체크하고 해야한다는거 같음. (Do not build Gradle task list during Gradle sync 해제 대신에.)
+
+        //위와 같이 하거나, Android Studio 터미널에서, ./gradlew tasks --all 명령어를 실행하여 해결할 수도 있다고 함.
+
+        //아무튼! 이게 지금 signingReport 클릭해서 키툴이다 라고 하는것을 통해서 얻어낸 SHA1 지문이다.
+        //이 SHA1 지문이 있어야, 구글 맵 키를 얻어낼 수 있다. ..(테스트용으론 이렇게간단하게 얻어낼 수 있음!)
+
+        //근데 정식 앱이었다면, 어플리케이션 빌드 하면서 싸인했던 키 가지고 얻어줘야 한다.
+
+
+
+
+        // 아무튼 이제 구글 개발자 사이트 이동하려면, 이게 매번 URL이 바뀌기도 해서. 구글 검색으로 가셈 -> google 검색 : "android map console" => 그럼 대충 https://developers.google.com/maps/documentation/android-sdk/get-api-key?hl=ko 이런 곳 나옴
+        // 진입하고 -> 시작하기
+        // 초기상태면 이것저것 설정한 뒤,
+        // 왼쪽 탭에 API 및 서비스 -> 라이브러리 OR 사용자 인증 정보    얘네만 보면됨. 지도 API만 가져다 쓴다라고 한다면
+        // 라이브러리에서 -> Maps SDK for Android -> 사용
+        // 다시 돌아와서 api 서비스 -> 사용자 인증 정보 -> 위쪽에 + 사용자 인증 정보 만들기 -> API 키 -> 키 제한
+        // 그럼, 하나의 키가 생성되었고 이 키를 등록하는건 맞긴 맞는데, 이걸 안드로이드 앱에서 사용하려면 키 제한을 걸어야 한다. (근데 나는 지금 키 제한이 안됨. 왜지? 무료맨이라 그런가)
+        // 그럼 이제 API 키 눌러서 키 제한사항 -> 앱 제한 설정 -> Android 앱 //API 제한사항 -> 키 제한 -> Select APIs -> Maps SDK for Android 체크
+        // 그다음, Android 제한사항 +(추가) -> 안드로이드 앱 추가에 패키지이름, SHA-1 인증서 디지털 지문 넣고 저장 => 사용 가능한 상태가 된다!
+        // 그럼 이제, API key 를 복사해서 Manifest 파일에 api 키 등록하는 곳에 복붙.
+
+//===============================================================================================================================================================================================
 /**
+ *
  * c83 ) GoogleMap
  *
  * * 위치 정보를 가져온 다음에, 위경도 값을 내부적으로만 이용할 수 도 있지만, 대부분의 경우 지도랑 많이 연동할 수 있다. -> 지도 띄우는 방법을 알자!
@@ -58,22 +113,26 @@ class MainActivity : AppCompatActivity() {
  *              />
  *
  *
+ * * 이제 지도를 얻는 방법! -> 콜백을 통해서 전달한다.
+ *
  * - GoogleMap 이라는 클래스가 지도를 출력
  *
  *          override fun onCreate(savedInstanceState: Bundle?) {
  *              super.onCreate(savedInstanceState)
  *              setContentView(R.layout.activity_main)
  *
- *              (supportFragmentManager.findFragmentById(R.id.mapView) as SupportMapFragment?)!!.getMapAsync(this)
+ *              (supportFragmentManager.findFragmentById(R.id.mapView) as SupportMapFragment?)!!.getMapAsync(this)              //R.id.mapView : 아까 지도를 등록한 프레그먼트... 여기에다가
+ *          }                                                                                                                   //getMapAsync 라고 등록을 해주면, 어느 순간에 지도가 이용 가능한 상태가 됐을 때 콜백을 해준다.
+ *                                                                                                                              //이 콜백이, 여기에 onMapReadt 라는 함수가 되겠다.
+ *
+ *          override fun onMapReady(p0: GoogleMap?) {                                                                           //얘 ㅇㅇ
+ *              googleMap = p0                                                                                                  //콜백을 등록하고 지도 가능한 상태로 됐을 때
+ *                                                                                                                              //이 콜백이 콜되면서 매개변수로 전달된다. (p0: GoogleMap?)
  *          }
  *
- *          override fun onMapReady(p0: GoogleMap?) {
- *              googleMap = p0
+ *          //결과적으로 이 매개변수로 전달되는 GoogleMap 이라는 객체가, 지도 객체라고 보면 된다.
  *
- *          }
- *
- *
- *
+ *          //실습에서, 위경도 설정, 지도의 센터위치 설정, 맵키 얻는 방법까지 정상적으로 따라해야 됨.
  *
  *
  *
