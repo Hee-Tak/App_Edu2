296c94004bbc8f74e5d5780cd3307d1719e0b68a
hitak <hitak77@naver.com>
2024-02-24 15:20:54 +0900

    앱 개발 학습 (인강) / GeoLocation - GoogleMap

---
+++ c83/src/main/java/com/tak/c83/MainActivity.kt
@@ -0,0 +1,80 @@
+package com.tak.c83
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
+
+/**
+ * c83 ) GoogleMap
+ *
+ * * 위치 정보를 가져온 다음에, 위경도 값을 내부적으로만 이용할 수 도 있지만, 대부분의 경우 지도랑 많이 연동할 수 있다. -> 지도 띄우는 방법을 알자!
+ * * 지도와 관련되어있는 것 -> 특정 벤더의 컨텐츠.
+ * * 안드로이드 앱을 개발하겟다라고 하면, 일차적으로, 어느 벤더의 콘텐츠를 이용할 지를 결정해줘야 한다.
+ * * 안드로이드 앱을 만든다라고 해서 꼭 구글 지도를 이용할 필요는 없다. 다른 벤더에서 제공되고 있는 지도를 이용해도 상관은 없다.
+ *
+ *
+ * * 지도를 이용하겠다라고 하면, 별도의 api를 이용해줘야 한다.
+ *
+ * <AndroidManifest.xml 설정>
+ *
+ *          implementation 'com.google.android.gms:play-services:12.0.1'                    // (이전 시간에서도 별도로 dependency로 설정해준 play-services api) -> 여기에서 지도를 핸들링할 수 있는 api를 제공한다. 그래서 dependency 관계 설정해줘야함.
+ *
+ *          // 지도 띄우기 위해 필요한 퍼미션들.
+ *          <uses-permission android:name="android.permission.ACCESS_NETWORK_STATE"/>       // 네트워킹을 하기 전에, 네트워크 상태 파악을 위해서 필요한 퍼미션. 직접 구현하는건 아니지만, 내부적으로 우리가 쓰는 API가 네트워크 상태 파악을 하기 때문에 달아줘야함.
+ *          <uses-permission android:name="android.permission.INTERNET"/>                   // 안드로이드 개발자에게 아주 유명한 퍼미션. 외부 서버랑 네트워킹할 때 필요하다. 여기서는 직접은아니지만, google 서버에 등록되어있는 map 을 끌어오기 위해 사용 (구글 서버와의 네트워킹)
+ *          <uses-permission android:name="android.permission.WRITE_EXTERNAL_STORAGE"/>     // 외장 메모리 공간에 파일을 write 할 때, 필요한 퍼미션. 우리가 하진 않지만, 내부적으로 이 api에서 구글 서버에서 받은 지도 데이터를 캐싱 차원에서 파일로 write 하기 때문에, 필요한 퍼미션.
+ *
+ *
+ *          // 추가적인 Manifest 파일 설정
+ *          <uses-library android:name="org.apache.http.legacy" android:required="false"/>  // 아파치 라이브러리를 쓰겠다. 선언 => 얘를 이용해서 많은 부분에서 네트워크 프로그램을 작성. 플레이서비스 버전에 따라서 여전히 아파치 라이브러리를 쓰고 있는 곳이 있을 수도 있다.
+ *          <meta-data android:name="com.google.android.maps.v2.API_KEY"                    //메타 정보를 매니페스트 파일에 선언   (이 부분 자체는 맵키가 등록되는 부분)
+ *              android:value="~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"/>
+ *          <meta-data android:name="com.google.android.gms.version"                        //몇버전의 라이브러리를 쓰는지.
+ *              android:value="@integer/google_play_services_version"/>
+ *
+ *          // 그리고, 지도는 벤더의 컨텐츠로, 그냥 넘겨줄 리가 없다.
+ *          // 그래서 그 벤더의 개발자 콘솔, 즉, 개발자 사이트에 가서 맵키(API_KEY)를 얻어내야 한다.
+ *          // 이 얻어진 키를 우리 앱에서 정상적으로 가지고 있어야 한다. 그래야지 정상적으로 지도 데이터가 넘어온다.
+ *
+ *
+ * * 지도도 화면이다. 화면을 구성하기 위해서는 UI를 구현하기 위한 뷰 View 가 필요하다.
+ *
+ * <GoogleMap>
+ *
+ * - play-service 라이브러리에서 지도는 프레그먼트로 제공         (이 지도 View 는, Fragment 로 제공한다.) (우리가 직접 작성한 프레그먼트는 아님) (api에서 제공해주는 프레그먼트)
+ *
+ *          <fragment
+ *              xmlns:android="http://schemas.android.com/apk/res/android"
+ *              android:id="@+id/mapView"
+ *              android:layout_width="match_parent"
+ *              android:layout_height="match_parent"
+ *              android:name="com.google.android.gms.maps.SupportMapFragment"       //api에서 제공해주고 있는 프레그먼트
+ *              />
+ *
+ *
+ * - GoogleMap 이라는 클래스가 지도를 출력
+ *
+ *          override fun onCreate(savedInstanceState: Bundle?) {
+ *              super.onCreate(savedInstanceState)
+ *              setContentView(R.layout.activity_main)
+ *
+ *              (supportFragmentManager.findFragmentById(R.id.mapView) as SupportMapFragment?)!!.getMapAsync(this)
+ *          }
+ *
+ *          override fun onMapReady(p0: GoogleMap?) {
+ *              googleMap = p0
+ *
+ *          }
+ *
+ *
+ *
+ *
+ *
+ *
+ */
